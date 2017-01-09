6c328a640817a4f87a1c1dc7ca8bb72eec655679
octacode <allblue.shasha@gmail.com>
2017-01-09 22:36:37 +0530

    Finished The Favourites activity. Improvements in the layout of tht Fav Activity are still needed.

--- app/src/main/java/octacode/allblue/code/moviezz/FavouritesActivity.java
+++ app/src/main/java/octacode/allblue/code/moviezz/FavouritesActivity.java
@@ -1,11 +1,14 @@
 package octacode.allblue.code.moviezz;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import octacode.allblue.code.moviezz.data.MovieContract;
@@ -14,6 +17,8 @@ import octacode.allblue.code.moviezz.data.MovieDbHelper;
 public class FavouritesActivity extends AppCompatActivity {
 
     String LOG_TAG = getClass().getSimpleName();
+
+    FavouritesAdapter favouritesAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +32,28 @@ public class FavouritesActivity extends AppCompatActivity {
                 null,
                 null,
                 null);
-        while(cursor.moveToNext()){
-            int title_index=cursor.getColumnIndex(MovieContract.FavouritesTable.COLUMN_MAIN_TITLE_TEXT);
-            Log.d(LOG_TAG,cursor.getString(title_index));
-        }
+        favouritesAdapter = new FavouritesAdapter(this,cursor);
+        fav_lv.setAdapter(favouritesAdapter);
+        fav_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                favouritesAdapter = (FavouritesAdapter)parent.getAdapter();
+                Cursor cursor = favouritesAdapter.getCursor();
+                Intent intent = new Intent(FavouritesActivity.this,DetailActivity2.class);
+                intent.putExtra("ID",cursor.getLong(MainFragment.COLUMN_ID));
+                intent.putExtra("MOVIE_ID",cursor.getString(MainFragment.COLUMN_MOVIE_ID));
+                intent.putExtra("ADULT",cursor.getString(MainFragment.COLUMN_ADULT));
+                intent.putExtra("BACKDROP_URL",cursor.getString(MainFragment.COLUMN_BACKDROP_URL));
+                intent.putExtra("OVERVIEW",cursor.getString(MainFragment.COLUMN_OVERVIEW));
+                intent.putExtra("TITLE",cursor.getString(MainFragment.COLUMN_TITLE));
+                intent.putExtra("POSTER_URL",cursor.getString(MainFragment.COLUMN_POSTER_URL));
+                intent.putExtra("LANGUAGE",cursor.getString(MainFragment.COLUMN_LANGUAGE));
+                intent.putExtra("POPULARITY",cursor.getString(MainFragment.COLUMN_POPULARITY));
+                intent.putExtra("VOTE_AVG",cursor.getString(MainFragment.COLUMN_VOTE_AVERAGE));
+                intent.putExtra("RATINGS",cursor.getString(MainFragment.COLUMN_RATINGS));
+                intent.putExtra("REL_DATE",cursor.getString(MainFragment.COLUMN_REL_DATE));
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/octacode/allblue/code/moviezz/FavouritesAdapter.java
@@ -0,0 +1,51 @@
+package octacode.allblue.code.moviezz;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by shasha on 9/1/17.
+ */
+
+public class FavouritesAdapter extends CursorAdapter {
+
+    public FavouritesAdapter(Context context,Cursor c) {
+        super(context,c);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.favourites_list,parent,false);
+        Holder viewHolder=new Holder(view);
+        view.setTag(viewHolder);
+        return view;
+    }
+    private Holder viewHolder;
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        viewHolder=(Holder)view.getTag();
+        String title = cursor.getString(MainFragment.COLUMN_TITLE);
+        final String postURL = cursor.getString(MainFragment.COLUMN_POSTER_URL);
+        viewHolder.ratings.setText(cursor.getString(MainFragment.COLUMN_VOTE_AVERAGE));
+        Picasso.with(context).load(postURL).error(R.mipmap.ic_launcher).into(viewHolder.thumbnail);
+        viewHolder.title.setText(title);
+    }
+
+    private class Holder{
+        private TextView title,ratings;
+        private ImageView thumbnail;
+        public Holder(View view){
+            thumbnail= (ImageView)view.findViewById(R.id.favourites_list_thumbnail);
+            title  = (TextView)view.findViewById(R.id.favourites_list_title);
+            ratings = (TextView)view.findViewById(R.id.favourites_list_ratings);
+        }
+    }
+}
